101edf8b6f6c4f0a0898e4672440884e29d36d7c
y3k <y3k00000@gmail.com>
2019-10-12 02:56:15 +0800

    test gradle dependency with Gson

--- app/src/main/java/y3k/testtravis/MainActivity.kt
+++ app/src/main/java/y3k/testtravis/MainActivity.kt
@@ -2,11 +2,15 @@ package y3k.testtravis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
+import android.utils.Log
+import com.google.gson.JsonObject
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("TestGson",Gson().fromJson("{a:1}",JsonObject::class.java)+"")
     }
 }
